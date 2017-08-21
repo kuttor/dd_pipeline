9ab739db3d9d543a3dd3697ec96d69fbfbf66485
Andrew <andrew.kuttor@gmail.com>
2017-08-21 14:35:19 -0700

    Added new command, Added comments

--- commands.sh
+++ commands.sh
@@ -9,10 +9,16 @@
 
 # Assigning networks Assigning networks to containers
 
+# Create network resource
 docker-create-network(){
   docker network create      \
     --subnet 10.1.0.0/16     \
     --gateway 10.1.0.1       \
     --ip-range=10.1.4.0/24
+}
+
+# List current network setup
+docker-network-ls(){
+  docker network ls
 }
 
