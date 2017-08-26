3b9f7645bc355256986a7c629091c3228b3ada85
Andrew <andrew.kuttor@gmail.com>
2017-08-26 14:20:59 -0700

    Added Elasticsearch section

---
+++ elasticsearch/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -
+#
+# title     :docker.sh
+# info      :Helper script for Dockerized Elasticsearch install
+# author    :Andrew Kuttor
+# date      :08-25-2017
+# version   :0.1
+# usage     :sh docker.sh
+# =============================================================================
+
+
+
